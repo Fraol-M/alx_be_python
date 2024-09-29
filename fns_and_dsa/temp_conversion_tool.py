9d71459c65ce65384681073248671fce5148797f
Fraol Megersa <fraolbacha111@gmail.com>
2024-09-29 14:54:17 +0300

    Commited fns and dsa file

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,26 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+
+def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    celsisu = (fahrenheit -32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsisu
+
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return fahrenheit
+
+
+temp = float(input("Enter the temperature to convert: "))
+check = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+match check:
+    case "F":
+        print(f"{temp}°F is {convert_to_celsius(temp)}°C") 
+    case "C":
+        print(f"{temp}°C is {convert_to_fahrenheit(temp)}°F")
+    case _:
+        print("Invalid Input")
+        
+        
