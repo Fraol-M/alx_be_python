37c4b08f65ad0141ddc4d7ca529f71e7f85d80bf
Fraol Megersa <fraolbacha111@gmail.com>
2024-10-12 22:20:28 +0300

    Fixed the bugs

--- oop/main.py
+++ oop/main.py
@@ -1,20 +1,20 @@
-# from book_class import Book
+from book_class import Book
 
-# def main():
-#     # Creating an instance of Book
-#     my_book = Book("1984", "George Orwell", 1949)
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
 
-#     # Demonstrating the __str__ method
-#     print(my_book)  # Expected to use __str__
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
 
-#     # Demonstrating the __repr__ method
-#     print(repr(my_book))  # Expected to use __repr__
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
 
-#     # Deleting a book instance to trigger __del__
-#     del my_book
+    # Deleting a book instance to trigger __del__
+    del my_book
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 
     
@@ -42,33 +42,33 @@ if __name__ == "__main__":
     
     
     
-# from polymorphism_demo import Shape, Rectangle, Circle
-# import math
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
 
-# def main():
-#     shapes = [
-#         Rectangle(10, 5),
-#         Circle(7)
-#     ]
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
 
-#     for shape in shapes:
-#         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
     
     
     
-# from class_static_methods_demo import Calculator
+from class_static_methods_demo import Calculator
 
-# def main():
-#     # Using the static method
-#     sum_result = Calculator.add(10, 5)
-#     print(f"The sum is: {sum_result}")
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
 
-#     # Using the class method
-#     product_result = Calculator.multiply(10, 5)
-#     print(f"The product is: {product_result}")
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
